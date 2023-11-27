9053d42d9021d5ee43a5b1818e669077807fcbbe
djibril soukouna <djib_cisse_9@hotmail.com>
2023-11-27 16:04:31 +0100

    job scrping ok + file created

--- scraper.js
+++ scraper.js
@@ -1,9 +1,22 @@
 const puppeteer = require("puppeteer");
+const moment = require("moment");
+// utiliser le "file system"
+const fs =require('fs').promises;
+
 
 const scrap = async () => {
   try {
     var browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
+
+    // to view the requests and select the ones you want
+    await page.setRequestInterception(true);
+    page.on("request", request => {
+      if (["stylesheet", "image", "font"].includes(request.resourceType()))
+        request.abort();
+      else request.continue();
+    });
+
     // URL where we needs data from with max elements
     await page.goto(
       "https://www.welcometothejungle.com/fr/jobs?refinementList%5Boffices.country_code%5D%5B%5D=FR&refinementList%5Boffices.state%5D%5B%5D=%C3%8Ele-de-France&refinementList%5Bcontract_type%5D%5B%5D=INTERNSHIP&refinementList%5Bcontract_type%5D%5B%5D=FULL_TIME&refinementList%5Bcontract_type%5D%5B%5D=TEMPORARY&query=d%C3%A9veloppeur%20javascript&page=1&aroundQuery=%C3%8Ele-de-France%2C%20France&sortBy=mostRecent", 
@@ -21,12 +34,38 @@ const scrap = async () => {
         return null;
       }
     );
+
+    const limitDate = moment().subtract(1, "days").toDate();
+    console.log(limitDate);
+
+    // $$eval c'est lorqu'il y a plusieur élément pour avoir un tableau
+    // if you want to use an argument, it has to be added after selector ($$eval(selector, pageFunction, args))
+    const jobList = await page.$$eval("li.ais-Hits-list-item", (arr, limitDate) => {
+      return arr.map( e => {
+        const url = e.querySelector("a").href;
+        const jobTitle = e.querySelector("h4").textContent.trim();
+        const tags = Array.from(
+          e.querySelectorAll("div[role='listitem']")
+          ).map(item => item.textContent.trim());
+        // pour utiliser il faut mettre dateTime avec T majuscule JS vanilla...
+        const createdAt = e.querySelector("time").dateTime;
+        
+        return { url, jobTitle, tags, createdAt };
+      }).filter(el => new Date(el.createdAt) > new Date(limitDate))
+    }, limitDate);
+
+    // create a file with data in json file :
+    await fs.writeFile(
+      `./jobs-${Date.now()}.json`,
+      JSON.stringify(jobList, null, 2));
+    // console.log(jobList);
     console.log(totalJobs);
+    console.log(jobList.length);
 
   } catch (error) {
     console.error(error);
   } finally {
-    // await browser.close();
+    await browser.close();
   }
 }
 
