72ef341ea66798e4128bf77db7100b692c69d042
djibril soukouna <djib_cisse_9@hotmail.com>
2023-12-03 23:52:53 +0100

    update URL

--- scraper.js
+++ scraper.js
@@ -20,7 +20,7 @@ const scrap = async () => {
 
     // URL where we needs data from with max elements
     await page.goto(
-      "https://www.welcometothejungle.com/fr/jobs?refinementList%5Boffices.country_code%5D%5B%5D=FR&refinementList%5Boffices.state%5D%5B%5D=%C3%8Ele-de-France&refinementList%5Bcontract_type%5D%5B%5D=INTERNSHIP&refinementList%5Bcontract_type%5D%5B%5D=FULL_TIME&refinementList%5Bcontract_type%5D%5B%5D=TEMPORARY&query=d%C3%A9veloppeur%20javascript&page=1&aroundQuery=%C3%8Ele-de-France%2C%20France&sortBy=mostRecent", 
+      "https://www.welcometothejungle.com/fr/jobs?refinementList%5Boffices.country_code%5D%5B%5D=FR&refinementList%5Boffices.state%5D%5B%5D=%C3%8Ele-de-France&refinementList%5Bcontract_type%5D%5B%5D=INTERNSHIP&refinementList%5Bcontract_type%5D%5B%5D=FULL_TIME&refinementList%5Bcontract_type%5D%5B%5D=TEMPORARY&query=d%C3%A9veloppeur%20react&page=1&aroundQuery=%C3%8Ele-de-France%2C%20France&sortBy=mostRecent", 
       {
         waitUntil : "networkidle2",  
       }
